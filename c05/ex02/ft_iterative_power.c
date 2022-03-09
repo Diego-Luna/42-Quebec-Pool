7d2d7e9a560436f72e1fdbadf555b11ccdb4471d
Diego Luna <49794452+Diego-Luna@users.noreply.github.com>
2022-03-08 19:29:44 -0500

    add ex00 to ex03

---
+++ c05/ex02/ft_iterative_power.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dluna-lo <dluna-lo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/08 18:12:14 by dluna-lo          #+#    #+#             */
+/*   Updated: 2022/03/08 18:35:21 by dluna-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	result;
+	int	i;
+
+	i = 0;
+	result = nb;
+	if (power < 0)
+		return (0);
+	if (nb == 0 && power == 0)
+		return (1);
+	while (i < power)
+	{
+		result *= nb;
+		i ++;
+	}
+	return (result);
+}
---
+++ c05/ex03/ft_recursive_power.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dluna-lo <dluna-lo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/08 18:36:33 by dluna-lo          #+#    #+#             */
+/*   Updated: 2022/03/08 19:29:01 by dluna-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (nb == 0 && power == 0)
+		return (1);
+	if (power == 0)
+		return (1);
+	return (ft_recursive_power(nb, power - 1) * nb);
+}
