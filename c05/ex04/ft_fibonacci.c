e26dff81566fd9010556b44af78497ad15ef0865
Diego Luna <49794452+Diego-Luna@users.noreply.github.com>
2022-03-09 12:12:55 -0500

    add ex04, c05

---
+++ c05/ex04/ft_fibonacci.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dluna-lo <dluna-lo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/08 19:37:46 by dluna-lo          #+#    #+#             */
+/*   Updated: 2022/03/09 09:04:30 by dluna-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	else
+		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
